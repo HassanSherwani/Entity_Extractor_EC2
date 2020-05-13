3ab307469090e29408085e100fd58905611cab32
HassanSherwani <45789548+HassanSherwani@users.noreply.github.com>
2020-05-13 04:12:53 +0200

    update script

--- flaskapp-docker/flaskapp/script_api.py
+++ flaskapp-docker/flaskapp/script_api.py
@@ -14,75 +14,33 @@ def extracts(raw_text):
 
 	nlp = spacy.load('en_core_web_sm', disable = ['ner'])
 
-	# TITLE
-	title1 = ["Agreement on Managed Data Center Services","Agreement on the Provision of Regional WAN IT Services"]
-	title2 = ["Master Services Agreement on the Provision of IT Services",
-			"Master Services Agreement on the Provision of IT Services (“Agreement“ or “Master Services Agreement”)"]
-	title3 = ["MASTER SERVICES AGREEMENT ON THE PROVISION OF MANAGED SERVICES IN PUBLIC COULDS",
-			"Master Services Agreement (“Agreement“ or “Master Services Agreement”) on the provision of Managed Services in Public Clouds"]
-	title4 = ["Agreement on the Provision of MANAGED PRINT Services",
-			"Agreement on the Provision of MPS (Managed Print Services)"]
-	title5 = ["Agreement for Security Operation Center Services"]
-	title6 = ["AGREEMENT ON PROVISIONING OF IT AND COMMUNICATION SERVICES"]
-	title7 = ["Agreement on Managed Data Center Services"]
-	title8 = ["Master Project, Support and Maintenance Agreement"]
-	title9 = ["ENTERPRISE CUSTOMER AGREEMENT","Master Project, Support and Maintenance Agreement"]
-	title10 = ["AGREEMENT on the provision of managed Mobile communication Services"]
-	title11 = ["MASTER SERVICE AGREEMENT","CONTRACT FOR TELECOMMUNICATION SERVICES"]
-	title12 = ["Agreement for Security Operation Center Services"]
-	title13 = ["Base Terms","AGREEMENT on the provision, OPERATION and maintenance of a CUSTOMER CONTACT PLATFORM",
-				"Agreement on the Provision, Operation and Maintenance of a Customer Contact Platform "]
-	title14 = ["AGREEMENT on the provision of hosting and support services For Macroware office 365"]
-
-
-	# SUPPLIER
-	suppliers1 = ["TEASYS", "Teasys", "TEASYS GLOBAL INVEST AG","Teasys International GmbH",
-				"Teasys Global Invest AG", "teasys global invest ag"]
-	suppliers2 = ["FTP", "FTP Deutschland GmbH", "FTP Deutschland GmbH"]
-	suppliers3 = ["Wisniewski & Sohn GmbH", "FBS"]
-	suppliers4 = ["Horizon Deutschland AG",
-				"Horizon", "Harpe", "Harpe Deutschland GmbH"]
-	suppliers5 = ["ADVENTURE SERVICES GMBH", "Adventure Services GmbH", "SWIPERO LIMITED", "Swipero Limited",
-				"Swipero"]
-	suppliers6 = ["Nozama Net Service", "NOZAMA NET SERVICE"]
-	suppliers7 = ["Schwyz Mail Solutions GmbH","Timber Limited"]
-	suppliers8 = ["Verizon Deutschland GmbH"]
-	suppliers9 = ["DZX","Avanti Netz GmbH","Baden Netz GmbH","e.com ScHolstein Telekommunikation GmbH",
-				"Netz AG","SCHOLSTEIN Netz AG"]
-
-
-	# CLIENT
-	clients = ["F.UN", "FUN", "F.UN BUSINESS SERVICES GMBH",
-			"F.UN Business Services GmbH"]
-
-	# DATE
-	dates1 = ["29 September 2018", "01 January 2015", "01.07.2018",
-			" August 2017", "6 December 2016", "December 2015","1 of July 2016" ]
-	dates2 = ["31. July 2018", "August 30, 2017"]
-	dates3 = ["period of 48 months", "36 months","concluded for five (5) years",
-			"three (3) months notice prior to termination"]
-	dates4 = ["31.01.2017", "31.03.2019", "1 October 2018", "September 1st, 2017"]
-	dates5 = ["31.12.2018", "Apr 11th 2023", "19.01.2020", "July 31, 2017"]
-
-
-	# COUNTRIES
-	countries1 = ["UK", "Germany", "France", "Italy","U.S","U.S.A","United States",
-				"Netherlands", "Russia", "Hungary", "India"]
-	countries2 = ["Slovakia", "Czech", "Australia","Cuba","North Sudan","North Korea",
-				"Vietnam", "Japan", "Philippines", "Romania"]
-	countries3 = ["Sweden", "Czech Republic", "United Kingdom", "Switzerland"]
-
-	# CLIENT_CONTRACT_MANAGER
-	cli_cont_managr1 = ["Amanda Kyzwani"]
-
-	# SUPPLIER_CONTRACT_MANAGER
-	supp_cont_manar1 = ["Tim Big"]
+	title1 = ["Agreement on contract1"]
+	title2 = ["Agreement on contract2"]
+	title3 = ["Agreement on contract3"]
+	title4 = ["Agreement on contract4"]
+	title5 = ["Agreement on contract5"]
+	title6 = ["Agreement on contract6"]
+	title7 = ["Agreement on contract7"]
+	title8 = ["Agreement on contract8"]
+	title9 = ["Agreement on contract9"]
+	suppliers1 = ["FACEBOOK", "Facebook", "FACEBOOK GLOBAL INVEST AG", "Facebook Global Invest AG"]
+	suppliers2 = ["BIRD", "BIRDS Deutschland GmbH", "Birds Deutschland GmbH"]
+	suppliers3 = ["Google GmbH", "GOOGLE"]
+	suppliers4 = ["EBAY Deutschland AG", "EBAY", "ebay"]
+	suppliers5 = ["AMAZON SERVICES GMBH", "Amazon Services GmbH", "AMAZON LIMITED", "Amazon Limited"]
+	clients = ["BOL.com", "bol.com", "BOL.COM BUSINESS SERVICES GMBH", "BOL.com Business Services GmbH"]
+	dates1 = ["29 September 2018", "01 January 2015", "01.07.2018", " August 2017"]
+	dates2 = ["31. July 2018"]
+	dates3 = ["termination after a period of 48 months"]
+	dates4 = ["31.01.2017", "31.03.2019", "1 October 2018"]
+	dates5 = ["31.12.2018", "Apr 11th 2023", "19.01.2020"]
+	countries1 = ["UK", "Germany", "France", "Italy", "Netherlands", "Russia", "Hungary", "India"]
+	countries2 = ["Slovakia", "Czech", "Australia", "Vietnam", "Japan", "Philippines", "Romania"]
 
 	rulerAll = EntityRuler(nlp, overwrite_ents=True)
 
-	# Add rulerAll to patterns
-
-	# For Title entity
+	# Add all patterns in respective entity
+	# Title
 
 	for tit1 in title1:
 		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit1}])
@@ -111,21 +69,6 @@ def extracts(raw_text):
 	for tit9 in title9:
 		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit9}])
 
-	for tit10 in title10:
-		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit10}])
-
-	for tit11 in title11:
-		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit11}])
-
-	for tit12 in title12:
-		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit12}])
-
-	for tit13 in title13:
-		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit13}])
-
-	for tit14 in title14:
-		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit14}])
-
 	# for supplier
 
 	for s1 in suppliers1:
@@ -142,17 +85,6 @@ def extracts(raw_text):
 
 	for s5 in suppliers5:
 		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s5}])
-
-	for s6 in suppliers6:
-		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s6}])
-
-	for s7 in suppliers7:
-		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s7}])
-
-	for s8 in suppliers8:
-		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s8}])
-	for s9 in suppliers9:
-		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s9}])
 
 	# for clients
 
@@ -184,18 +116,92 @@ def extracts(raw_text):
 	for count2 in countries2:
 		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count2}])
 
-	for count3 in countries3:
-		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count3}])
+	# Add rulerAll to patterns
 
-	# CLIENT_CONTRACT_MANAGER
+	rulerAll = EntityRuler(nlp, overwrite_ents=True)
 
-	for c_mangr in cli_cont_managr1:
-		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": c_mangr}])
 
-	# SUPPLIER_CONTRACT_MANAGER
 
-	for supp_mangr in supp_cont_manar1:
-		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": supp_mangr}])
+	# For Title entity
+
+	for tit1 in title1:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit1}])
+
+	for tit2 in title2:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit2}])
+
+	for tit3 in title3:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit3}])
+
+	for tit4 in title4:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit4}])
+
+	for tit5 in title5:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit5}])
+
+	for tit6 in title6:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit6}])
+
+	for tit7 in title7:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit7}])
+
+	for tit8 in title8:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit8}])
+
+	for tit9 in title9:
+		rulerAll.add_patterns([{"label": "TITLE", "pattern": tit9}])
+
+
+	# for supplier
+
+	for s1 in suppliers1:
+		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s1}])
+
+	for s2 in suppliers2:
+		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s2}])
+
+	for s3 in suppliers3:
+		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s3}])
+
+	for s4 in suppliers4:
+		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s4}])
+
+	for s5 in suppliers5:
+		rulerAll.add_patterns([{"label": "SUPPLIER", "pattern": s5}])
+
+
+	# for clients
+
+	for c1 in clients:
+		rulerAll.add_patterns([{"label": "CLIENT", "pattern": c1}])
+
+	# Pattern for DATES
+
+	for t1 in dates1:
+		rulerAll.add_patterns([{"label": "Effective-DATES", "pattern": t1}])
+
+	for t2 in dates2:
+		rulerAll.add_patterns([{"label": "Signature-DATES", "pattern": t2}])
+
+	for t3 in dates3:
+		rulerAll.add_patterns([{"label": "Termination-DATES", "pattern": t3}])
+
+	for t4 in dates4:
+		rulerAll.add_patterns([{"label": "Commencement-DATES", "pattern": t4}])
+
+	for t5 in dates5:
+		rulerAll.add_patterns([{"label": "END-DATES", "pattern": t5}])
+
+	# for countries
+
+	for count1 in countries1:
+		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count1}])
+
+	for count2 in countries2:
+		rulerAll.add_patterns([{"label": "COUNTRIES", "pattern": count2}])
+
+
+
 
 	# HOLISTIC NAME FOR RULER
 	rulerAll.name = 'rulerAll'
